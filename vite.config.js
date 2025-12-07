4fe43b0ced82cc049827ff9c3bc6c42811951212
Nabin Saud <nsaud@usf.edu>
2025-12-07 04:13:48 -0500

    Simplify Vite config

--- vite.config.js
+++ vite.config.js
@@ -1,10 +1,8 @@
 import { defineConfig } from "vite";
-import react from "@vitejs/plugin-react";
 
 const repoName = "TFE_website";
 const basePath = process.env.GITHUB_ACTIONS ? `/${repoName}/` : "/";
 
 export default defineConfig({
   base: basePath,
-  plugins: [react()],
 });
